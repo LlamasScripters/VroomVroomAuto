3f48de2c91b671daf6fc573752785abe607c751f
aria.aman <ariaaman@outlook.fr>
2025-01-01 22:26:34 +0100

    Ajout de tailwind CSS

---
+++ infrastructure/platforms/react/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+    content: [
+      "./index.html", // Chemin vers ton fichier index.html
+      "./src/**/*.{js,jsx,ts,tsx}", // Tous les fichiers React dans src
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
