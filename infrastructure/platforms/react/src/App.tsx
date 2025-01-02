9f128ec58bafb949a08ae45233cef44b1b04df96
iSaaMz <sami.assiakh@gmail.com>
2025-01-02 03:40:43 +0100

    fix conflit app.tsx

--- infrastructure/platforms/react/src/App.tsx
+++ infrastructure/platforms/react/src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import routes from './routes';
 
@@ -6,14 +5,6 @@ const router = createBrowserRouter(routes);
 
 function App() {
   return <RouterProvider router={router} />;
-=======
-export default function App() {
-  return (
-    <div>
-      
-    </div>
-  );
->>>>>>> 3531898a7f873c6fe30f41a78d2ecf596b806d43
 }
 
 export default App;
