8e7618df8c6e118349bd7b211166c4f048205d40
aria.aman <ariaaman@outlook.fr>
2025-01-19 23:53:27 +0100

    Déplacement du package .json global dans le domain, update UserRepository.ts, UserCrudCases

--- application/repositories/UserRepository.ts
+++ application/repositories/UserRepository.ts
@@ -1,9 +1,11 @@
 import { User } from '../../domain/entities/UserEntity';
 import { UUID } from '../../domain/value-objects/UUID';
+import { Email } from '../../domain/value-objects/EMAIL';
 
 export interface UserRepository {
   save(user: User): Promise<User>;
   findById(userId: UUID): Promise<User | null>;
+  findByEmail(email: Email): Promise<User | null>;
   delete(userId: UUID): Promise<boolean>;
   findAll(): Promise<User[]>;
   update(user: User): Promise<User>;
--- application/usecases/user/UserCrudUseCases.ts
+++ application/usecases/user/UserCrudUseCases.ts
@@ -3,43 +3,116 @@ import { UserRepository } from '../../repositories/UserRepository';
 import { UUID } from '../../../domain/value-objects/UUID';
 import { Email } from '../../../domain/value-objects/EMAIL';
 import { Password } from '../../../domain/value-objects/PASSWORD';
+import { Role } from '@domain/value-objects/ROLE';
+import { CreateUserDTO, UpdateUserDTO, GetUserDTO } from '@application/dtos/UserDTO';
+import { Username } from '@domain/value-objects/USERNAME';
+import { UserResponse } from '@application/response/UserResponse';
+import { AuthenticationService } from '@application/services/AuthentificationService';
+import { PasswordService } from '@application/services/PasswordService';
 
-interface UserData {
-  userId: UUID;
-  username: string;
-  email: Email;
-  motDePasse: Password;
-  role: string;
-}
+export class UserCrudUseCases {
+  constructor(
+    private userRepository: UserRepository,
+    private authenticationService: AuthenticationService,
+    private passwordService: PasswordService
+  ) {}
 
-export class UserUseCases {
-  constructor(private userRepository: UserRepository) {}
+  async createUser(token: string, userData: CreateUserDTO): Promise<UserResponse> {
 
-  async createUser(username: string, email: Email, motDePasse: Password, role: string): Promise<User> {
-    const user = User.create(new UUID(), username, email, motDePasse, role);
-    return this.userRepository.save(user);
+    const requesterId = await this.authenticationService.verifyAuthenticationToken(token);
+    if (!requesterId) throw new Error('Invalid token');
+
+    
+    const username = new Username(userData.username);
+    const email = new Email(userData.email);
+    const hashedPassword = await this.passwordService.hashPassword(userData.password); 
+    
+
+    const user = User.create(
+      new UUID(),
+      username,
+      email,
+      new Password(hashedPassword),
+      new Role('USER'),
+      true,
+      new Date(),
+      new Date()
+    );
+
+    try {
+      const savedUser = await this.userRepository.save(user);
+      return {userId: savedUser.userId.toString()};
+    } catch (error) {
+      throw error;
+    }
   }
 
-  async getUserById(userId: UUID): Promise<User | null> {
-    return this.userRepository.findById(userId);
+  async getUserById(token: string, userData: GetUserDTO): Promise<User | null> {
+    const userIdentifier = new UUID(userData.userId);
+    
+    const requesterId = await this.authenticationService.verifyAuthenticationToken(token);
+    if (!requesterId) throw new Error('Invalid token');
+    
+    return await this.userRepository.findById(userIdentifier);
   }
 
-  async updateUser(userId: UUID, updatedData: Partial<UserData>): Promise<User | null> {
-    const user = await this.userRepository.findById(userId);
+  async updateUser(token: string, updatedData: UpdateUserDTO): Promise<User | null> {
+    let username: Username | undefined;
+    let email: Email | undefined;
+    let password: Password | undefined;
+    let role: Role | undefined;
+    let isValidated: boolean | undefined;
+
+      if (updatedData.username) {
+        username = new Username(updatedData.username);
+      }
+      if (updatedData.email) {
+        email = new Email(updatedData.email);
+      } 
+      if (updatedData.password) {
+        password = new Password(updatedData.password);
+      }
+      if (updatedData.isValidated) {
+          isValidated = updatedData.isValidated;
+      }
+
+      const requesterId = await this.authenticationService.verifyAuthenticationToken(token);
+      if (!requesterId) throw new Error('Invalid token');
+
+
+    const userIdentifier = new UUID(updatedData.userId);
+    const user = await this.userRepository.findById(userIdentifier);
     if (!user) return null;
 
     const updatedUser = User.create(
       user.userId,
-      updatedData.username || user.username,
-      updatedData.email || user.email,
-      updatedData.motDePasse || user.motDePasse,
-      updatedData.role || user.role
+      username || user.username,
+      email || user.email,
+      password || user.password,
+      role || user.role,
+      isValidated || user.isValidated,
+      user.dateCreation,
+      user.derniereConnexion
     );
     
-    return this.userRepository.save(updatedUser);
+    return await this.userRepository.update(updatedUser);
   }
 
-  async deleteUser(userId: UUID): Promise<boolean> {
-    return this.userRepository.delete(userId);
+  async deleteUser(token: string, userId: string): Promise<boolean> {
+    const userIdentifier = new UUID(userId);
+
+    const requesterId = await this.authenticationService.verifyAuthenticationToken(token);
+    if (!requesterId) throw new Error('Invalid token');
+
+    return await this.userRepository.delete(userIdentifier);
   }
+
+  async getAllUsers(token: string): Promise<User[]> {
+
+    const requesterId = await this.authenticationService.verifyAuthenticationToken(token);
+    if (!requesterId) throw new Error('Invalid token');
+    
+    return await this.userRepository.findAll();
+  }
+
 }
