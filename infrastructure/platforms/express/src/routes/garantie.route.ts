7d77c95f70762292964e4534f63129735ffb60ba
MoussaST <moussa21sr@gmail.com>
2025-02-04 15:17:21 +0100

    Protection de la route create de la garantie

--- infrastructure/platforms/express/src/routes/garantie.route.ts
+++ infrastructure/platforms/express/src/routes/garantie.route.ts
@@ -7,8 +7,7 @@ const controller = new GarantieController();
 
 router.get('/', authenticate, authorizeAdminOrGestionnaire, (req, res) => controller.getAllGaranties(req, res));
 router.get('/:id',authenticate, authorizeAdminOrGestionnaire, (req, res) => controller.getGarantieById(req, res));
-//router.post('/',authenticate, authorizeAdminOrGestionnaire, (req, res) => controller.createGarantie(req, res));
-router.post('/', (req, res) => controller.createGarantie(req, res));
+router.post('/',authenticate, authorizeAdminOrGestionnaire, (req, res) => controller.createGarantie(req, res));
 router.put('/:id',authenticate, authorizeAdminOrGestionnaire, (req, res) => controller.updateGarantie(req, res));
 router.delete('/:id',authenticate, authorizeAdminOrGestionnaire, (req, res) => controller.deleteGarantie(req, res));
 
