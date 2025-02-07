abd94d01fd93bc69bfc467fe649d86d76abc8ed6
MoussaST <moussa21sr@gmail.com>
2025-02-07 21:22:59 +0100

    Sécurisation de la route essaie de l'API

--- infrastructure/platforms/express/src/routes/essaie.route.ts
+++ infrastructure/platforms/express/src/routes/essaie.route.ts
@@ -7,7 +7,7 @@ const controller = new EssaiController();
 
 router.get('/', authenticate, authorizeAdminOrGestionnaire, (req, res) => controller.getAllEssais(req, res));
 router.get('/:id',authenticate, authorizeAdminOrGestionnaire, (req, res) => controller.getEssaiById(req, res));
-router.post('/', (req, res) => controller.createEssai(req, res));
+router.post('/',authenticate, authorizeAdminOrGestionnaire, (req, res) => controller.createEssai(req, res));
 router.put('/:id',authenticate, authorizeAdminOrGestionnaire, (req, res) => controller.updateEssai(req, res));
 router.delete('/:id',authenticate, authorizeAdminOrGestionnaire, (req, res) => controller.deleteEssai(req, res));
 
