f91750e1020e1f73dfdcc65a2c11f8fc60cc0452
aria.aman <ariaaman@outlook.fr>
2025-02-10 22:38:06 +0100

    Fix authorisation des routes

--- infrastructure/platforms/express/src/routes/entretien.route.ts
+++ infrastructure/platforms/express/src/routes/entretien.route.ts
@@ -1,12 +1,12 @@
 // infrastructure/platforms/express/src/routes/entretien.route.ts
 import { Router } from 'express';
 import { EntretienController } from '../controllers/entretienController';
-import { authenticate, authorizeAdminOrGestionnaire,authorizeAdmin, authorizeUser,authorizeGestionnaire } from '../middlewares/authMiddleware';
+import { authenticate, authorizeAdminOrGestionnaire} from '../middlewares/authMiddleware';
 const router = Router();
 const controller = new EntretienController();
 
-router.get('/',authenticate, authorizeAdmin,authorizeUser, authorizeGestionnaire, (req, res) => controller.getAllEntretiens(req, res));
-router.get('/:id',authenticate, authorizeAdmin, authorizeUser, authorizeGestionnaire, (req, res) => controller.getEntretienById(req, res));
+router.get('/',authenticate, authorizeAdminOrGestionnaire, (req, res) => controller.getAllEntretiens(req, res));
+router.get('/:id',authenticate, (req, res) => controller.getEntretienById(req, res));
 router.post('/',authenticate, authorizeAdminOrGestionnaire, (req, res) => controller.createEntretien(req, res));
 router.put('/:id',authenticate, authorizeAdminOrGestionnaire, (req, res) => controller.updateEntretien(req, res));
 router.delete('/:id',authenticate, authorizeAdminOrGestionnaire, (req, res) => controller.deleteEntretien(req, res));
--- infrastructure/platforms/express/src/routes/moto.route.ts
+++ infrastructure/platforms/express/src/routes/moto.route.ts
@@ -2,13 +2,13 @@
 
 import { Router } from 'express';
 import { MotoController } from '../controllers/motoController';
-import { authenticate, authorizeAdminOrGestionnaire, authorizeAdmin,authorizeUser, authorizeGestionnaire } from '../middlewares/authMiddleware';
+import { authenticate, authorizeAdminOrGestionnaire } from '../middlewares/authMiddleware';
 
 const router = Router();
 const motoController = new MotoController();
 
-router.post('/',authenticate, authorizeAdmin,authorizeUser, authorizeGestionnaire, (req, res) => motoController.createMoto(req, res));
-router.get('/',authenticate, authorizeAdmin,authorizeUser, authorizeGestionnaire, (req, res) => motoController.getAllMotos(req, res));
+router.post('/',authenticate, authorizeAdminOrGestionnaire, (req, res) => motoController.createMoto(req, res));
+router.get('/',authenticate,  (req, res) => motoController.getAllMotos(req, res));
 router.get('/:id',authenticate, authorizeAdminOrGestionnaire, (req, res) => motoController.getMoto(req, res));
 router.put('/:id',authenticate, authorizeAdminOrGestionnaire, (req, res) => motoController.updateMoto(req, res));
 router.delete('/:id',authenticate, authorizeAdminOrGestionnaire, (req, res) => motoController.deleteMoto(req, res));
